fc0ce21d330915c81b102f5221bfa4999be04483
jjaey1 <jjaey1@naver.com>
2020-01-14 15:16:22 +0900

    first commit in branch

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestClass{
     public static void main (Stirng[] args){
-        System.out.println("Hello world!");
+        System.out.println("Hi world!");
     }
 }
