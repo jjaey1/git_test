8b9600b719fe566df2bb9e641d01688b86c255d5
jjaey1 <jjaey1@naver.com>
2020-01-14 15:19:12 +0900

    second commit in branch

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,7 @@ package test;
 
 public class TestClass{
     public static void main (Stirng[] args){
-        System.out.println("Hi world!");
+        System.out.println("Hi");
+        System.out.println("world!");
     }
 }
