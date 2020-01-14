9f0c326013e9f83ff2fc4810b8db48df085a7197
jjaey1 <45361452+jjaey1@users.noreply.github.com>
2020-01-14 16:08:29 +0900

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -3,6 +3,6 @@ package test;
 public class TestClass{
     public static void main (Stirng[] args){
         System.out.println("Hi~~");
-        System.out.println("world!");
+        System.out.println("world!~!~!");
     }
 }
